74f811b51ddeac3bd30e80ca69bee151354c2a75
dowd7 <102552320+dowd7@users.noreply.github.com>
2022-05-31 12:15:20 -0700

    added update and HttpResponseDto

--- src/main/java/com/revature/Revamedia/beans/controllers/CommentController.java
+++ src/main/java/com/revature/Revamedia/beans/controllers/CommentController.java
@@ -1,8 +1,12 @@
 package com.revature.Revamedia.beans.controllers;
 
 import com.revature.Revamedia.beans.services.UserCommentsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.Revamedia.dtos.HttpResponseDto;
+import com.revature.Revamedia.entities.UserComments;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/comment")
@@ -14,4 +18,21 @@ public class CommentController {
     public CommentController(UserCommentsService userCommentsService) {
         this.userCommentsService = userCommentsService;
     }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.OK)
+    public HttpResponseDto updateById(@RequestBody UserComments updatedComment, HttpServletResponse res) {
+        UserComments comment = userCommentsService.update(updatedComment);
+
+        if(comment.getMessage() != comment.getMessage()) {
+            res.setStatus(400);
+            return new HttpResponseDto(400, "Failed to update category", comment);
+        } else {
+            res.setStatus(200);
+            return new HttpResponseDto(200, "Successfully updated category" + comment.getMessage(), comment);
+        }
+    }
+
+
 }
+

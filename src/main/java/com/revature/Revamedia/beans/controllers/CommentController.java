19a406183b48629eea87d3c4b843fffab941871f
Diego Leon <diego042@revature.net>
2022-05-31 12:16:27 -0700

    getById() method started

--- src/main/java/com/revature/Revamedia/beans/controllers/CommentController.java
+++ src/main/java/com/revature/Revamedia/beans/controllers/CommentController.java
@@ -1,8 +1,12 @@
 package com.revature.Revamedia.beans.controllers;
 
 import com.revature.Revamedia.beans.services.UserCommentsService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.Revamedia.entities.UserComments;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/comment")
@@ -11,7 +15,15 @@ public class CommentController {
     // Initialize Service
     private final UserCommentsService userCommentsService;
 
+    @Autowired
     public CommentController(UserCommentsService userCommentsService) {
         this.userCommentsService = userCommentsService;
     }
+
+    @GetMapping("/{id}")
+    public UserComments getById(HttpServletResponse response, @PathVariable("id") int id) {
+
+        return this.userCommentsService.getCommentById(id);;
+    }
+
 }

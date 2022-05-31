1907a61b4becd06b84c126de6d6131a7b46dd593
Diego Leon <diego042@revature.net>
2022-05-31 12:22:04 -0700

    Updated getById() method to return the HttpResponseDto

--- src/main/java/com/revature/Revamedia/beans/controllers/CommentController.java
+++ src/main/java/com/revature/Revamedia/beans/controllers/CommentController.java
@@ -1,6 +1,7 @@
 package com.revature.Revamedia.beans.controllers;
 
 import com.revature.Revamedia.beans.services.UserCommentsService;
+import com.revature.Revamedia.dtos.HttpResponseDto;
 import com.revature.Revamedia.entities.UserComments;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -22,9 +23,17 @@ public class CommentController {
     }
 
     @GetMapping("/{id}")
-    public UserComments getById(HttpServletResponse response, @PathVariable("id") int id) {
+    @ResponseStatus(HttpStatus.OK)
+    public HttpResponseDto getById(HttpServletResponse res, @PathVariable("id") int id) {
+        UserComments comment = userCommentsService.getCommentById(id);
 
-        return this.userCommentsService.getCommentById(id);;
+        if(comment.getCommentId() != id) {
+            res.setStatus(400);
+            return new HttpResponseDto(400, "Failed to get comment.", comment);
+        } else {
+            res.setStatus(200);
+            return new HttpResponseDto(200, "Successfully retrieved comment." + comment.getMessage(), comment);
+        }
     }
 
 

84a3fcb5cec50bb893c568fe1852947a5747ce17
Laflammex <92762772+GioAmira@users.noreply.github.com>
2022-06-01 21:58:38 -0400

    added documentation

--- src/main/java/com/revature/Revamedia/beans/controllers/AuthController.java
+++ src/main/java/com/revature/Revamedia/beans/controllers/AuthController.java
@@ -2,15 +2,17 @@ package com.revature.Revamedia.beans.controllers;
 
 import com.revature.Revamedia.beans.services.AuthService;
 import com.revature.Revamedia.dtos.UserRegisterDto;
-import com.revature.Revamedia.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 /**
- * @Author: Giorgi Amirajibi, Mohammad Foroutanyazdian, Fatemeh Goudarzi, Tony Henderson
- * @Contributor: Kenneth Strohm, Randall Hale
+ * @Author(s): Giorgi Amirajibi, Mohammad Foroutanyazdian, Fatemeh Goudarzi, Tony Henderson
+ * @Contributor(s): Kenneth Strohm, Randall Hale
+ * Purpose: It is used to receive/produce http requests/responses.
+ *          This controller specifically deals with User registration and login.
+ *          Uses AuthService object, that is injected in a constructor, to invoke CRUD functionality.
  */
 
 @RestController
--- src/main/java/com/revature/Revamedia/dtos/UserRegisterDto.java
+++ src/main/java/com/revature/Revamedia/dtos/UserRegisterDto.java
@@ -5,6 +5,8 @@ import javax.validation.constraints.*;
 /**
  * @Author: Giorgi Amirajibi, Mohammad Foroutanyazdian, Fatemeh Goudarzi, Tony Henderson
  * @Contributor: Kenneth Strohm, Randall Hale
+ * Purpose: Object of this class is used to register a user.
+ *          It contains and validates all the necessary fields that are filled during registration.
  */
 public class UserRegisterDto {
 

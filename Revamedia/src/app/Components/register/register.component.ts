8aa2f73db2aef0ece33bcf2762dd51b019a26abe
Fgoudarzi <goudarzi2002@gmail.com>
2022-06-02 10:24:40 -0500

    register frontend

--- Revamedia/src/app/Components/register/register.component.ts
+++ Revamedia/src/app/Components/register/register.component.ts
@@ -1,6 +1,8 @@
+import { RegisterService } from './../../services/register.service';
 import { Component, OnInit } from '@angular/core';
 // Icons
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +11,31 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private register : RegisterService) { }
+  username: string = "";
+  password: string = "";
+  fName: string = "";
+  lName: string = "";
+  email: string = "";
 
   ngOnInit(): void {
   }
+
+  registerHandler(username: string, password: string, fName: string, lName: string, email: string){
+
+
+      let user = new User(username, password, fName, lName, email);
+      console.log(user)
+      let options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      }
+      this.register.createUser(user, options).subscribe((data) => {
+        console.log(data)
+    })
+    }
+
 
   // Front End Work
   public faEye = faEye; // icons
@@ -26,5 +49,24 @@ export class RegisterComponent implements OnInit {
   public showConfirmPassword = false;
   public toggleShowConfirmPassword(){
     this.showConfirmPassword = !this.showConfirmPassword;
+  }
+}
+
+
+export class User {
+  id: number | undefined;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+
+  constructor(_username: string, _password: string, _fName: string, _lName: string, _email: string) {
+    this.username = _username;
+    this.password = _password;
+    this.firstName = _fName;
+    this.lastName = _lName;
+    this.email = _email;
+
   }
 }

7349dfd4a4f701658af34d7de22e6e118708f340
leobarrientos02 <leobarrientos02@gmail.com>
2022-06-01 20:26:50 -0400

    dark mode

--- Revamedia/src/app/Components/settings/settings.component.ts
+++ Revamedia/src/app/Components/settings/settings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// Icons
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-settings',
@@ -12,7 +14,12 @@ export class SettingsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // Front End Work
+  public faSun = faSun; // icon
+  public faMoon = faMoon; // icon
+  public darkTheme = false;
   public changeTheme() {
-    document.body.classList.add('darkMode');
+    document.body.classList.toggle('darkMode');
+    this.darkTheme = !this.darkTheme;
   }
 }

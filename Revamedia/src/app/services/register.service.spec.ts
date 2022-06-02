fd3a5144f9de3782c05fbcc7f5c9b3e9d5b0aab8
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-06-02 14:18:01 -0500

    .

--- Revamedia/src/app/services/register.service.spec.ts
+++ Revamedia/src/app/services/register.service.spec.ts
@@ -1,32 +1,33 @@
 import { HttpClient } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { RegisterService } from './register.service';
 
 describe('RegisterService', () => {
-  let service: RegisterService;
-  let HttpClientSpy: { post: jasmine.Spy } = jasmine.createSpyObj('HttpClient', ['post']);
 
+  let registerService: RegisterService;
+  let baseUrl;
 
   beforeEach(() => {
-
-    HttpClientSpy.post.and.returnValue(1);
-
     TestBed.configureTestingModule({
       providers: [
-        { provide: HttpClient, useValue: HttpClientSpy }
+        HttpClient,
+        HttpTestingController
       ]
     });
-    service = TestBed.inject(RegisterService);
+    registerService = TestBed.inject(RegisterService);
+
+    baseUrl = registerService.baseUrl;
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(registerService).toBeTruthy();
   });
 
   describe("#createUser", () => {
 
-    it("Should create and return a observable to register", () => {
+    it("Should create and return an Http request observable", () => {
 
     });
   })

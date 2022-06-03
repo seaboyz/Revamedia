f50cab55d2b3213d3593a2803af386a2795b5929
Jarod H <38392785+DuhChosenNoodle@users.noreply.github.com>
2022-06-03 11:48:00 -0700

    diverging devops jarod with devops

--- Revamedia/src/environments/environment.prod.ts
+++ Revamedia/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: '220328p3revamedia-env.eba-mczbwbpi.us-east-1.elasticbeanstalk.com',
-  apiUrl_Jarod: 'http://revamedia-env.eba-v2eppq3f.us-west-1.elasticbeanstalk.com/',
+  baseUrl: 'http://revamedia-env.eba-v2eppq3f.us-west-1.elasticbeanstalk.com/',
   apiKey: 'M4g0SX27EqOQZ9eAipfpXcGPrfYAQsrI'
 };

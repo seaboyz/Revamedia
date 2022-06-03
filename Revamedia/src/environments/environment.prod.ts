8f3bb1d91f02b47ff4fee1a07c30465d74f52deb
Jarod H <38392785+DuhChosenNoodle@users.noreply.github.com>
2022-06-03 08:03:21 -0700

    devops build
    should build successfully now
    
    pom update, buildspec update
    
    cors filter
    
    Update buildspec.yml and angular file size limits
    
    Create buildspec-rev.yml

--- Revamedia/src/environments/environment.prod.ts
+++ Revamedia/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '',
-  apiUrl_Jarod: 'http://revamedia-env.eba-v2eppq3f.us-west-1.elasticbeanstalk.com/'
+  baseUrl: '220328p3revamedia-env.eba-mczbwbpi.us-east-1.elasticbeanstalk.com',
+  apiUrl_Jarod: 'http://revamedia-env.eba-v2eppq3f.us-west-1.elasticbeanstalk.com/',
+  apiKey: 'M4g0SX27EqOQZ9eAipfpXcGPrfYAQsrI'
 };

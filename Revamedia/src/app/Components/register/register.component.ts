d6ac86a44ce570fa40d8fda067a82a674d7d456e
seaboyz <seaboyz@hotmail.com>
2022-06-05 12:59:00 -0500

    Change register() in register component to fire-auth

--- Revamedia/src/app/Components/register/register.component.ts
+++ Revamedia/src/app/Components/register/register.component.ts
@@ -5,15 +5,17 @@ import { Router } from '@angular/router';
 import { RegisterService } from '../../Shared/services/register-service/register.service';
 import { IRegisterError } from '../../Shared/interfaces/IRegisterError.interface';
 import { IUserInterface } from '../../Shared/interfaces/IUserInterface';
+import { FireAuthService } from "src/app/Shared/services/fire-auth-service/fire-auth.service";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit
+{
 
-  constructor(private register: RegisterService, private router: Router) { }
+  constructor(private auth: FireAuthService, private router: Router) { }
   username: string = "";
   password: string = "";
   confirmPassword: string = "";
@@ -21,13 +23,13 @@ export class RegisterComponent implements OnInit {
   lName: string = "";
   email: string = "";
 
-  error : IRegisterError = {
-    errorStatus :"",
-    errorFirstName : "",
-    errorLastName : "",
-    errorEmail : "",
-    errorUsername : "",
-    errorPassword : ""
+  error: IRegisterError = {
+    errorStatus: "",
+    errorFirstName: "",
+    errorLastName: "",
+    errorEmail: "",
+    errorUsername: "",
+    errorPassword: ""
   };
 
   public user: IUserInterface = {
@@ -39,10 +41,12 @@ export class RegisterComponent implements OnInit {
     email: ''
   };
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
   }
 
-  registerHandler(username: string, password: string, fName: string, lName: string, email: string) {
+  registerHandler(username: string, password: string, fName: string, lName: string, email: string)
+  {
     this.user.firstName = fName;
     this.user.lastName = lName;
     this.user.username = username;
@@ -54,42 +58,43 @@ export class RegisterComponent implements OnInit {
         'Content-Type': 'application/json'
       })
     }
-    this.register.createUser(this.user, options).subscribe((data) => {
-    
-      this.router.navigateByUrl('/login')
-      
-    },(error) => {this.error = error}
-    )
+
+    this.auth.register(this.email, this.password)
+      .then(data =>
+        this.router.navigateByUrl('/login'))
+      // todo: error handling
+      .catch(error => console.log(error))
   }
 
-  checkInput(data: string) {
-    const input : any = document.querySelector("#" + data);
-    if(!input.checkValidity()){
-      switch(data) {
-        case "fName" : this.error.errorFirstName = "First name should consist of letters only \nand minimum two characters";
-        break;
-        case "lName" : this.error.errorLastName = "Last name should consist of letters only \nand minimum two characters";
-        break;
-        case "email" : this.error.errorEmail = "Not a valid email";
-        break;
-        case "username" : this.error.errorUsername = "Username should be between 2 and 255 characters";
-        break;
-        case "password" : this.error.errorPassword= "Min 8 characters, at least 1 uppercase letter, 1 lowercase, 1 number and 1 special character";
-        break;
+  checkInput(data: string)
+  {
+    const input: any = document.querySelector("#" + data);
+    if (!input.checkValidity()) {
+      switch (data) {
+        case "fName": this.error.errorFirstName = "First name should consist of letters only \nand minimum two characters";
+          break;
+        case "lName": this.error.errorLastName = "Last name should consist of letters only \nand minimum two characters";
+          break;
+        case "email": this.error.errorEmail = "Not a valid email";
+          break;
+        case "username": this.error.errorUsername = "Username should be between 2 and 255 characters";
+          break;
+        case "password": this.error.errorPassword = "Min 8 characters, at least 1 uppercase letter, 1 lowercase, 1 number and 1 special character";
+          break;
       }
     }
-    else{
-      switch(data) {
-        case "fName" : this.error.errorFirstName = "";
-        break;
-        case "lName" : this.error.errorLastName = "";
-        break;
-        case "email" : this.error.errorEmail = "";
-        break;
-        case "username" : this.error.errorUsername = "";
-        break;
-        case "password" : this.error.errorPassword= "";
-        break;
+    else {
+      switch (data) {
+        case "fName": this.error.errorFirstName = "";
+          break;
+        case "lName": this.error.errorLastName = "";
+          break;
+        case "email": this.error.errorEmail = "";
+          break;
+        case "username": this.error.errorUsername = "";
+          break;
+        case "password": this.error.errorPassword = "";
+          break;
       }
     }
   }
@@ -101,11 +106,13 @@ export class RegisterComponent implements OnInit {
 
   // Show Password
   public showPassword = false;
-  public toggleShowPassword() {
+  public toggleShowPassword()
+  {
     this.showPassword = !this.showPassword;
   }
   public showConfirmPassword = false;
-  public toggleShowConfirmPassword() {
+  public toggleShowConfirmPassword()
+  {
     this.showConfirmPassword = !this.showConfirmPassword;
   }
 }

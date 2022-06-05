96aecd28b72e132cf418af5548856536f3ab8b0b
seaboyz <seaboyz@hotmail.com>
2022-06-05 11:58:42 -0500

    Add fire-auth.service.ts

--- Revamedia/src/app/Shared/services/fire-auth-service/fire-auth.service.ts
+++ Revamedia/src/app/Shared/services/fire-auth-service/fire-auth.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, signOut, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { NgForm } from "@angular/forms";
 
 @Injectable({
   providedIn: 'root'
 })
-export class FireAuthService {
+export class FireAuthService
+{
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  login(email: string, password: string)
+  {
+
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  logout()
+  {
+    signOut(this.auth);
+  }
+
+  register(email: string, password: string)
+  {
+    return createUserWithEmailAndPassword(this.auth, email, password)
+  }
+
+
+
 }

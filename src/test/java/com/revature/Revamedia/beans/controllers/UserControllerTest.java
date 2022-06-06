dc75c7b20da4493974adccb0b68cb5aad133969e
Simmons Banana <simmons@thinkful.com>
2022-06-06 12:31:55 -0700

    userController broke code no backc reference

--- src/test/java/com/revature/Revamedia/beans/controllers/UserControllerTest.java
+++ src/test/java/com/revature/Revamedia/beans/controllers/UserControllerTest.java
@@ -1,6 +1,8 @@
 package com.revature.Revamedia.beans.controllers;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.Revamedia.beans.services.UserService;
 import com.revature.Revamedia.dtos.ViewAllUserDto;
@@ -13,12 +15,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -40,27 +45,29 @@ public class UserControllerTest {
     }
 
     @Test
-    public void getAllUsersTest() {
+    public void getAllUsersTest() throws Exception {
         //arrange
         //ViewAllUserDto viewAllUserDto = new ViewAllUserDto();
-        List<User> users = new ArrayList<>();
-        HttpHeaders httpHeaders = new HttpHeaders();
+        //List<User> users = new ArrayList<>();
+        //HttpHeaders httpHeaders = new HttpHeaders();
         //httpHeaders.add("Set-Cookie", "user_session=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJKc29uIjoie1widXNlcklkXCI6NCxcInVzZXJuYW1lXCI6XCJzaGFkeVwifSJ9.LSzPbhNAALEFrBWZPpf8KGvREormRNt3tXFkGMTvnU3-MPHw76JD5cmreZJMYaSwNt7H6YJlALCFAWobPAKWbw; Max-Age=86400; Path=/;");
 
         // this part solves following casting problem
         //org.springframework.http.ResponseEntity cannot be cast to class java.util.List
-        ResponseEntity<List<User>> responseEntity = ResponseEntity.ok().headers(httpHeaders).build();
-        List<User> returnedUsers = responseEntity.getBody();
+        //ResponseEntity<List<User>> responseEntity = ResponseEntity.ok().build();
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
         //-----------------------------------------------------------
-        when(userServiceMock.getAllUsers()).thenReturn((returnedUsers));
+        when(userServiceMock.getAllUsers()).thenReturn((users));
         ObjectMapper mapper = new ObjectMapper();
-        try {
             //act
-            String content = mapper.writeValueAsString(users);
-            this.mockMvc.perform(get("/user/allUsers").contentType(MediaType.APPLICATION_JSON).content(content)).andExpect(status().isOk());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+            //String content = mapper.writeValueAsString(users);
+            MvcResult result = this.mockMvc.perform(get("/user/allUsers").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+            System.out.println(result.getResponse().getContentAsString());
+            List<User> returnedUsers = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<User>>(){});
+            //List<User> returnedUsers1 = mapper.convertValue(returnedUsers, new List<User>[]);
+            assertEquals(returnedUsers, users.toString());
     }
 
     @Test

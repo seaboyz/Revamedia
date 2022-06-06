6577b171ee088ccc80859bf74462bfdc53f98604
seaboyz <seaboyz@hotmail.com>
2022-06-06 07:03:02 -0500

    Remove angular fire from message service

--- Revamedia/src/app/Shared/services/message-service/message.service.ts
+++ Revamedia/src/app/Shared/services/message-service/message.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 
 
 import { Message } from "../../interfaces/message";
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 import { Observable } from "rxjs";
 
@@ -14,25 +12,15 @@ import { Observable } from "rxjs";
 })
 export class MessageService
 {
-  private messagesCollection: AngularFirestoreCollection<Message>;
-  messages: Observable<Message[]>;
 
-  constructor(private afs: AngularFirestore, private auth: AngularFireAuth)
+
+  constructor()
   {
-    this.messagesCollection = afs.collection<Message>('messages');
-    this.messages = this.messagesCollection.valueChanges();
+
   }
 
   sendMessage(message: string)
   {
-    // const timestamp = this.getTimeStramp();
-    // const email = this.user.email;
-    // this.messages = this.getMessages();
-    // this.messages.push({
-    //   message: message,
-    //   timeSent: timestamp,
-    //   username: this.userName,
-    //   email: email
-    // })
+    console.log("sending message: ", message)
   }
 }

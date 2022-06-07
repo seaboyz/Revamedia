0732ed8209f65a3694c989044cd087bb62e8c98e
leobarrientos02 <leobarrientos02@gmail.com>
2022-06-07 11:30:38 -0400

    GUARD UP
    Guard is to protect users from going searching through resources by changing the url

--- Revamedia/src/app/app-routing.module.ts
+++ Revamedia/src/app/app-routing.module.ts
@@ -9,16 +9,17 @@ import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { ErrorPageComponent } from './Components/error-page/error-page.component';
 import { ProfileComponent } from './Components/profile/profile.component';
+import {AuthGuard} from './Shared/guard/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'messages', component: MessagesComponent },
-  { path: 'events', component: EventsComponent },
-  { path: 'groups', component: GroupsComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'home', component: HomeComponent, canActivate:[AuthGuard]},
+  { path: 'messages', component: MessagesComponent,canActivate:[AuthGuard]},
+  // { path: 'events', component: EventsComponent },
+  // { path: 'groups', component: GroupsComponent },
+  { path: 'settings', component: SettingsComponent,canActivate:[AuthGuard]},
+  { path: 'profile', component: ProfileComponent,canActivate:[AuthGuard]},
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: ErrorPageComponent },
 ];
